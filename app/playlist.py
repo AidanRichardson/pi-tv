ed701680076b3e1d19f6ba90aeaf872ae20709b4
Aidan Richardson <aidan.richardson@me.com>
2026-06-29 23:30:05 +0100

    fix: fixing reading m3u file on windows

--- app/playlist.py
+++ app/playlist.py
@@ -4,7 +4,7 @@ from app.db import save_channel
 
 
 def parse_m3u(db, path: str):
-    with open(path) as f:
+    with open(path, "r", encoding="utf-8", errors="ignore") as f:
         lines = f.readlines()
 
     total = sum(1 for l in lines if "#EXTINF" in l)
@@ -16,8 +16,10 @@ def parse_m3u(db, path: str):
             channel = {}
             for value in x:
                 channel[value[0]] = value[1]
-            url_split = lines[i + 1].strip().split("/")
-            channel["ts"] = url_split[-1]
+            if i + 1 < len(lines):
+                url_split = lines[i + 1].strip().split("/")
+                channel["ts"] = url_split[-1]
+
             save_channel(db, channel)
             count += 1
             yield int((count / total) * 100)
